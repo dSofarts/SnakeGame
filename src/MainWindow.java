d502ae71bd00b154fe694859e2ec89614301a004
Egor <DronovEgorVl@yandex.com>
2021-06-22 22:50:28 +0300

    Create constructor

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,2 +1,16 @@
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Snake Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(520,545);
+        setLocation(600, 600);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
 }
