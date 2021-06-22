daca7e98fe8738f2410e02bada891e865b5f43d0
Egor <DronovEgorVl@yandex.com>
2021-06-22 23:57:04 +0300

    create move detection

--- src/GameField.java
+++ src/GameField.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
@@ -17,7 +19,7 @@ public class GameField extends JPanel implements ActionListener {
     private int dots;
     private Timer timer;
     private boolean left = false;
-    private boolean right = false;
+    private boolean right = true;
     private boolean up = false;
     private boolean down = false;
     private boolean inGame = true;
@@ -26,7 +28,7 @@ public class GameField extends JPanel implements ActionListener {
     public GameField() {
         setBackground(Color.black);
         loadImages();
-
+        initGame();
     }
     public void initGame() {
         dots = 2;
@@ -51,6 +53,17 @@ public class GameField extends JPanel implements ActionListener {
         dot = iid.getImage();
     }
 
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot,x[i],y[i],this);
+            }
+        }
+    }
+
     public void move() {
         for (int i = dots; i > 0; i--) {
             x[i] = x[i - 1];
@@ -70,11 +83,69 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            dots++;
+            createApple();
+        }
+    }
+
+    public void checkCollisions() {
+        for (int i = dots; i > 0; i--) {
+            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+                inGame = false;
+            }
+        }
+        if (x[0] > SIZE) {
+            inGame = false;
+        }
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
+            inGame = false;
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (inGame) {
+            checkApple();
+            checkCollisions();
             move();
         }
         repaint();
+    }
+
+    class FieldKeyListener extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT && !right) {
+                left = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_RIGHT && !left) {
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_UP && !down) {
+                up = true;
+                left = false;
+                right = false;
+            }
+            if (key == KeyEvent.VK_DOWN && !up) {
+                down = true;
+                left = false;
+                right = false;
+            }
+        }
     }
 }
