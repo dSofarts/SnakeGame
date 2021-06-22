05a5e9d531ae84d7e9817fd0014226d04f8bdcb1
Egor <DronovEgorVl@yandex.com>
2021-06-22 23:13:42 +0300

    load images

--- src/GameField.java
+++ src/GameField.java
@@ -21,6 +21,13 @@ public class GameField extends JPanel {
 
 
     public GameField() {
-
+        setBackground(Color.black);
+        loadImages();
+    }
+    public void loadImages() {
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
     }
 }
